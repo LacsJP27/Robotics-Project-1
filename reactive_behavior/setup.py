19dceb5e930cb5746a21972281589a115bf3b493
Avinash Kandadi <137839526+Avikandadi@users.noreply.github.com>
2026-02-25 19:50:32 +0000

    reactive behavior package

---
+++ reactive_behavior/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'reactive_behavior'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kand0007',
+    maintainer_email='kand0007@todo.todo',
+    description='Reactive behavior controller (Phase 2)',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller = reactive_behavior.controller_node:main',
+        ],
+    },
+)
